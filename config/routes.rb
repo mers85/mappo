7c0b6f2c7d93233db3783612d713e55d010b54ad
Mayra Rodriguez <mers85@gmail.com>
2018-07-24 06:42:26 +0200

    vue.js installed and working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'landing/index'
   namespace :api, defaults: { format: "json" } do
       namespace :v1 do
         resources :maps
       end
   end
+
+  root to: 'landing#index'
 end
